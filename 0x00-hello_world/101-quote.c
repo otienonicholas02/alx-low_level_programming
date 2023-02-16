9aac57bd42404580da60dd6a45914c1ea8c7ae61
otienonicholas02 <otienonicholas02@gmail.com>
2023-02-16 05:03:15 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,7 @@
 *
 *Return: return 1
 */
-int main(void)
-{
-	fput(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+int main(void){
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n ,stdout);
 	return (1);
 }
