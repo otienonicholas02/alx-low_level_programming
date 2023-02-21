b03052fbae7c4d43f89ec37169e0a3d07e36cf2c
otienonicholas02 <otienonicholas02@gmail.com>
2023-02-21 12:24:52 -0800

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "main.h"
+
 /**
  * main - Entry point
  *
@@ -8,6 +7,23 @@
  */
 int main(void)
 {
+	int i = 0;
+	long j = 1, k = 2;
 
+	while (i < 50)
+	{
+		if (i == 0)
+			printf("%ld", j);
+		else if (i == 1)
+			printf(", %ld", k);
+		else
+		{
+			k += j;
+			j = k - k;
+			printf(", %ld", k);
+		}
+		++i;
+	}
+	printf("\n");
 	return (0);
 }
