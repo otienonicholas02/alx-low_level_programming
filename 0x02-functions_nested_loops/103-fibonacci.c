711807e69d4f179c8cfa28bb55367572830e00f4
otienonicholas02 <otienonicholas02@gmail.com>
2023-02-21 12:33:16 -0800

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "main.h"
+
 /**
  * main - Entry point
  *
@@ -8,6 +7,17 @@
  */
 int main(void)
 {
+	int i = 0;
+	long j = 1, k = 2, sum = k;
 
+	while (k + j < 4000000)
+	{
+		k += j;
+		if (k % 2 == 0)
+			sum += k;
+		j = k - j;
+		++i;
+	}
+	printf("%ld\n", sum);
 	return (0);
 }
