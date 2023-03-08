a5a196ad4d4b71d001a4f378214e48f382eff061
otienonicholas02 <otienonicholas02@gmail.com>
2023-03-08 11:36:49 -0800

    update

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,14 +10,10 @@
  * @s: The string to be printed.                                                                                                        
  */                                                                                                                                     
 void _puts_recursion(char *s)                                                                                                           
-{                                                                                                                                       
-        if (*s)                                                                                                                         
-        {                                                                                                                               
-                _putchar(*s);                                                                                                           
-                _puts_recursion(s + 1);                                                                                                 
-        }                                                                                                                               
-                                                                                                                                        
-        else                                                                                                                            
-                _putchar('\n');                                                                                                         
+{                                                                                      if (*s)                                                                                                                         
+        {                                                                                       _putchar(*s);                                                                                                           
+                _puts_recursion(s + 1);                                                 }                                                                                                                               
+              else                                                                                                                           
+                _putchar('\n');                                               
 }
 
