dbe32ff6edbd8350bdfc98c0942b693b9946be72
otienonicholas02 <otienonicholas02@gmail.com>
2023-03-08 13:04:03 -0800

    update

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,28 +1,18 @@
-/*
- * File: 4-pow_recursion.c
- */
-
 #include "main.h"
 
 /**
- * _pow_recursion - Returns the value of x raised to the power of y.
- * @x: The number to be raised.
- * @y: The power.
+ * _pow_recursion - raises x to the power of y
+ * @x: Number Integer
+ * @y: Power
  *
- * Return: The value of x raised to the power of y.
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _pow_recursion(int x, int y)
 {
-        int result = x;
-
-        if (y < 0)
-                return (-1);
-
-        else if (y == 0)
-                return (1);
-
-        result *= _pow_recursion(x, y - 1);
-
-        return (result);
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
 }
-
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,29 +1,91 @@
-/*
- * File: 6-is_prime_number.c
-
- */
-
 #include "main.h"
 
-int is_divisible(int num, int div);
-int is_prime_number(int n);
+/**
+ * prime2 - Makes possible to evaluate from 1 to n
+ * @a: same number as n
+ * @b: number that iterates from 1 to n
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int prime2(int a, int b)
+{
+	if (a == b)
+		return (1);
+	else if (a % b == 0)
+		return (0);
+	return (prime2(a, b + 1));
+}
+/**
+ * is_prime_number - checks if a number is prime
+ * @n: Number Integer
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (prime2(n, 2));
+}#include "holberton.h"
 
 /**
- * is_divisible - Checks if a number is divisible.
- * @num: The number to be checked.
- * @div: The divisor.
+ * prime2 - Makes possible to evaluate from 1 to n
+ * @a: same number as n
+ * @b: number that iterates from 1 to n
  *
- * Return: If the number is divisible - 0.
- *         If the number is not divisible - 1.
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int is_divisible(int num, int div)
+int prime2(int a, int b)
 {
-        if (num % div == 0)
-                return (0);
-
-        if (div == num / 2)
-                return (1);
-
-        return (is_divisible(num, div + 1));
+	if (a == b)
+		return (1);
+	else if (a % b == 0)
+		return (0);
+	return (prime2(a, b + 1));
 }
+/**
+ * is_prime_number - checks if a number is prime
+ * @n: Number Integer
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (prime2(n, 2));
+}#include "holberton.h"
 
+/**
+ * prime2 - Makes possible to evaluate from 1 to n
+ * @a: same number as n
+ * @b: number that iterates from 1 to n
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int prime2(int a, int b)
+{
+	if (a == b)
+		return (1);
+	else if (a % b == 0)
+		return (0);
+	return (prime2(a, b + 1));
+}
+/**
+ * is_prime_number - checks if a number is prime
+ * @n: Number Integer
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (prime2(n, 2));
+}
