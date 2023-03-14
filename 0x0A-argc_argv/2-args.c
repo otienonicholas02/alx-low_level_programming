dc204e97b78026f9cc4881756c7093b9d7746b3b
otienonicholas02 <otienonicholas02@gmail.com>
2023-03-14 14:17:12 -0700

    update

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -4,36 +4,22 @@
 
 
 /**
-
- * main - prints all arguments it receives
-
- * @argc: number of arguments
-
- * @argv: array of arguments
-
- *
-
- * Return: Always 0 (Success)
-
- */
+* main - prints all arguments it r
+*
+*@argc: number of arguments
+* @argv: array of arguments
+*
+* Return: Always 0 (Success)
+*/
 
 int main(int argc, char *argv[])
-
 {
-
-        int i;
-
-
-        for (i = 0; i < argc; i++)
-
-        {
-
-                printf("%s\n", argv[i]);
-
-        }
-
-
-        return (0);
-
+int i;
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
 }
+return (0);
+}
+
 
