6e8266fe0261bdea9658486de0272d91bae9aaf4
otienonicholas02 <otienonicholas02@gmail.com>
2023-03-20 12:10:34 -0700

    update

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
 
 /**
- * main - prints the name of the file
- * Return: Always 0 (Success)
- */
-int ain(void)
+  * print_dog - Prints all the data of a dog
+  * @d: A dog structure
+  *
+  * Return: Nothing
+  */
+void print_dog(struct dog *d)
 {
-	printf("%s\n", __FILE__);
-	return (0);
+	if (d != NULL)
+	{
+		printf("Name: %s\n", (d->name) ? d->name : "(nil)");
+		printf("Age: %f\n", (d->age) ? d->age : 0);
+		printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
+	}
 }
-
