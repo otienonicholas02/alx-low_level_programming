eb3d33ae1c6943a0e0cbe73bb0311cefdc1bb6d0
otienonicholas02 <otienonicholas02@gmail.com>
2023-03-20 09:56:43 -0700

    0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+	#include "dog.h"
+
+
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+	void free_dog(dog_t *d)
+	{
+		if (d)
+		{
+			free(d->name);
+			free(d->owner);
+			free(d);
+		}
+	}
+
