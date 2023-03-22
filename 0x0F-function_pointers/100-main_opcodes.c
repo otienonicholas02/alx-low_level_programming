fd8b21dda423c46688fcb96dfa55aec71e6cbaac
otienonicholas02 <otienonicholas02@gmail.com>
2023-03-22 13:07:54 -0700

    update

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -21,6 +21,20 @@ bytes = atoi(argv[1]);
 if (bytes < 0)
 {
 printf("Error\n");
+exit(2);
 }
+arr = (char *)main;
+for (i = 0; i < bytes; i++)
+{
+if (i == bytes - 1)
+{
+printf("%02hhx\n", arr[i]);
+break;
 }
+printf("%02hhx ", arr[i]);
+}
+return (0);
+}
+
+
 
